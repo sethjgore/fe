d2941c9663730febfe2c2f0213a33395f3597a0f
sethjgore <sethjgore@gmail.com>
2013-08-09 10:44:32 -0500

    fix nav title size to 1.5em

--- config.rb
+++ config.rb
@@ -1,57 +1,10 @@
-###
-# Compass
-###
 
+# ROUTER IMPLEMENTATION VIA MIDDLEMAN RACKWARE
+page "/",               :layout => :layout
+page "/*",              :layout => :general
+page "/projects.html",  :layout => :project
 
-
-# Susy grids in Compass
-# First: gem install susy
-# require 'susy'
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-
-page "/about.html", :layout => :project
-page "/projects.html", :layout => :project
-# Proxy (fake) files
-# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
-#   @which_fake_page = "Rendering a fake page with a variable"
-# end
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
+#SET DIRECTORIES OF IMG / JS / CSS
 set :css_dir, 'css'
 
 set :js_dir, 'js'
@@ -60,19 +13,19 @@ set :images_dir, 'img'
 
 activate :livereload
 
-# Build-specific configuration
+#WHEN BUILDING SITE, DODO? 
 configure :build do
   # For example, change the Compass output style for deployment
-  #activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  #activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
-  #activate :cache_buster
+  activate :cache_buster
 
   # Use relative URLs
- activate :relative_assets
+  activate :relative_assets
 
   activate :directory_indexes
 
@@ -85,22 +38,27 @@ configure :build do
   # set :http_path, "/Content/images/"
 end
 
+#WHEN LAND PROJECT/* DODO? && BLOG PREFERENCES
 activate :blog do |blog|
-  #blog.prefix = "projects"
- # blog.permalink = ":title"
-  #blog.layout = "layouts/blog"
-  #blog.sources = "projects/"
 
   blog.prefix = "projects"
   blog.permalink = ":title"
-  blog.layout = "blog"
+  blog.layout = "project-stand-alone"
   blog.default_extension = ".md"
+
 end
 
+# TURNS ON RELATIVE ASSETS. DO NOT CHANGE
 activate :relative_assets
+
+# TURNS ON VANITY URLS. DO NOT CHANGE. 
 activate :directory_indexes
+
+# TURNS ON RELATIVE LINKING. DO NOT CHANGE.
 set :relative_links, true
 
+# IN BROWSER EDITING PREFERENCES. DO NOT CHANGE. 
+# DOES NOT WORK AT THE MOMENT.
+# WAIT UNTIL COMPASS ACCEPTS SASS BLEEDING EDGE
 sass_options = {:debug_info => true}
-
 sass_options = {:sourcemap => true}
---
+++ source/projects/2013-08-08-projecthumanity.html.md
@@ -0,0 +1,6 @@
+---
+title: Project Humanity
+date: 2013-08-08 00:34 UTC
+tags:
+---
+
